bc6092278989d6be1582d9d7a9084d467f10a85f
gugbab2 <jkhoo1997@gmail.com>
2023-07-09 12:30:20 +0900

    project setting

---
+++ commerce/src/main/java/flab/project/commerce/CommerceApplication.java
@@ -0,0 +1,13 @@
+package flab.project.commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerceApplication.class, args);
+	}
+
+}
